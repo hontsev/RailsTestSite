f227e741497ec441aaa41d9f545c3925bd545dff
Lais Hontsev <hontsev@gmail.com>
2017-11-06 05:36:30 +0000

    add high-score model

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :high_scores
   get 'greetings/hello'
   get 'greetings/goodbye'
   # The priority is based upon order of creation: first created -> highest priority.
